180586689ade0244ed0c1185206f3797a2524bee
Alan <alan.tapscott@gmail.com>
2018-09-24 19:05:12 +0200

    Added secured user, key, value association

--- api.py
+++ api.py
@@ -9,6 +9,7 @@ https://github.com/miguelgrinberg/REST-auth
 """
 
 import os
+import json
 from flask import Flask, abort, request, jsonify, g, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
@@ -18,7 +19,7 @@ from itsdangerous import (TimedJSONWebSignatureSerializer
 
 # initialization
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SECRET_KEY'] = 'the god machine knows all but gives out nothing'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
@@ -55,6 +56,16 @@ class User(db.Model):
         user = User.query.get(data['id'])
         return user
 
+class Key(db.Model):
+    __tablename__ = 'keys'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), index=True)
+
+class UserKeyValue(db.Model):
+    __tablename__ = 'user_key_value'
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    key_id = db.Column(db.Integer, db.ForeignKey('keys.id'), primary_key=True)
+    value = db.Column(db.String(50))
 
 @auth.verify_password
 def verify_password(username_or_token, password):
@@ -84,7 +95,6 @@ def new_user():
     return (jsonify({'username': user.username}), 201,
             {'Location': url_for('get_user', id=user.id, _external=True)})
 
-
 @app.route('/api/users/<int:id>')
 def get_user(id):
     user = User.query.get(id)
@@ -100,22 +110,59 @@ def get_auth_token():
     return jsonify({'token': token.decode('ascii'), 'duration': 600})
 
 
-@app.route('/api/resource')
+@app.route('/api/values', methods=['GET'])
 @auth.login_required
-def get_resource():
-    return jsonify({'data': 'Hello, %s!' % g.user.username})
+def get_values():
+    user_key_values = UserKeyValue.query.filter_by(user_id=g.user.id).all()
+    key_values = []
+    for ukv in user_key_values:
+        key = Key.query.filter_by(id=ukv.key_id).first()
+        key_values.append([key.name, ukv.value])
+
+    return jsonify({'data': key_values})
+
+@app.route('/api/values', methods=['PUT'])
+@auth.login_required
+def update_values():
+    username = request.json.get('username')
+    user = User.query.filter_by(username=username).first()
+    key_values = request.json.get('key_values')
+    for key_name, value in key_values.items():
+        key = Key.query.filter_by(name=key_name).first()
+        user_key_value = UserKeyValue(user_id=user.id, key_id=key.id, value=value)
+        db.session.add(user_key_value)
+        db.session.commit()
+
+    return jsonify({'data': str(g.user.__dict__)})
 
 
 @app.route('/main', methods=['GET'])
+@auth.login_required
 def main():
-    return_string = ""
+    return_string = "Users<br></br>"
     for u in [(u.id, u.username) for u in User.query.all()]:
         return_string += '{}<br></br>'.format(str(u))
+    return_string += 'Keys<br></br>'
+    for k in Key.query.all():
+        return_string += 'Id:{} Name:{}<br></br>'.format(k.id, k.name)
+    return_string += 'UserKeyValues<br></br>'
+    for ukv in UserKeyValue.query.all():
+        return_string += 'User:{} Key:{} Value:{}<br></br>'.format(ukv.user_id, ukv.key_id, ukv.value)
     return return_string
+
+
+def initialize():
+    db.create_all()
+    with open('settings.json', encoding='utf-8') as f:
+        setting_data = json.load(f)
+        for key_name in setting_data['keys']:
+            key = Key(name=key_name)
+            db.session.add(key)
+            db.session.commit()
 
 
 if __name__ == '__main__':
     if not os.path.exists('db.sqlite'):
-        db.create_all()
+        initialize()
     # app.run(debug=True)
     app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
--- utils.py
+++ utils.py
@@ -14,10 +14,23 @@ def create_user(username, password):
     print(r.text)
 
 
-def get_protected_URI(username, password):
-    r = requests.get(url="{}{}".format(API_ENDPOINT, "/api/resource"), auth=(username, password))
+def get_values(username, password):
+    r = requests.get(url="{}{}".format(API_ENDPOINT, "/api/values"), auth=(username, password))
     print(r)
+
+def update_values(username, password, key_values):
+
+    data = {'username': username, 'key_values': key_values}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+    r = requests.put(url="{}{}".format(API_ENDPOINT, "/api/values"), data=json.dumps(data), headers=headers,
+                     auth=(username, password))
+
+    print(r.text)
 
 
 if __name__ == '__main__':
-    create_user('alan', 'password')
+    # create_user('alan', 'password')
+    update_values('alan', 'password', {'k1': 'v1', 'k2': 'v2'})
+    # r = get_values('alan', 'password')
+    pass
