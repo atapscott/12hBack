479b3790c8e1eb1a7a5c71d85e8e9ac1774e519f
atapscott <alan.tapscott@gmail.com>
2018-10-10 16:52:39 +0200

    Dynamic interface for method invocation

--- utils.py
+++ utils.py
@@ -1,36 +1,71 @@
 import requests
 import json
+import sys
 
 API_ENDPOINT = "http://127.0.0.1:5000"
 
 
-def create_user(username, password):
+def create_user(*args):
+    username = args[0][0]
+    password = args[0][1]
 
     data = {'username': username, 'password': password}
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
     r = requests.post(url="{}{}".format(API_ENDPOINT, "/api/users"), data=json.dumps(data), headers=headers)
 
-    print(r.text)
+    return r
 
 
-def get_values(username, password):
-    r = requests.get(url="{}{}".format(API_ENDPOINT, "/api/values"), auth=(username, password))
-    print(r)
-
-def update_values(username, password, key_values):
-
-    data = {'username': username, 'key_values': key_values}
+def get_key_values(*args):
+    username = args[0][0]
+    password = args[0][1]
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-    r = requests.put(url="{}{}".format(API_ENDPOINT, "/api/values"), data=json.dumps(data), headers=headers,
+    r = requests.get(url="{}{}".format(API_ENDPOINT, "/api/keyvalues"), auth=(username, password), headers=headers)
+    return r
+
+
+def purge_data(*args):
+    username = args[0][0]
+    password = args[0][1]
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+    r = requests.post(url="{}{}".format(API_ENDPOINT, "/api/purge"), auth=(username, password), headers=headers)
+    return r
+
+
+def update_value(*args):
+    username = args[0][0]
+    password = args[0][1]
+    key = args[0][2]
+    value = args[0][3]
+
+    data = {'value': value}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+    r = requests.put(url="{}{}{}".format(API_ENDPOINT, "/api/keyvalue/", key), data=json.dumps(data), headers=headers,
                      auth=(username, password))
 
-    print(r.text)
+    return r
+
+
+def get_key_value(*args):
+    username = args[0][0]
+    password = args[0][1]
+    key = args[0][2]
+
+    headers = {'Accept': 'text/plain'}
+
+    r = requests.get(url="{}{}{}".format(API_ENDPOINT, "/api/keyvalue/", key), headers=headers,
+                     auth=(username, password))
+
+    return r
 
 
 if __name__ == '__main__':
-    # create_user('alan', 'password')
-    update_values('alan', 'password', {'k1': 'v1', 'k2': 'v2'})
-    # r = get_values('alan', 'password')
+
+    method = eval(sys.argv[1])
+    r = method(sys.argv[2:])
+    print(r.text)
     pass
